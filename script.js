78215662c427952bb1a0ed49e93a1d0fd4575c74
Sahil Singh <sahils091@gmail.com>
2021-01-13 22:48:53 -0500

    leet code RegEx  13/01/2020

--- script.js
+++ script.js
@@ -1526,3 +1526,18 @@ const isMatch = function(s, p) {
 // "c*a*b"
 // "mississippi"
 // "mis*is*p*."
+
+
+
+
+//Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+const reverse = (x) => {
+  let y = 0, res = 0;
+  while (x) {
+      res = x%10;
+      x = parseInt(x/10);
+      y = y * 10 + res;
+  }
+  return y < Math.pow(-2, 31) || Math.pow(2, 31) < y ? 0 : y;
+};
