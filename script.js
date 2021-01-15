54be09c2333b7323add5e326b7c12e05ac421c98
Sahil Singh <sahils091@gmail.com>
2021-01-14 21:47:03 -0500

    leet code RegEx  14/01/2020

--- script.js
+++ script.js
@@ -1541,3 +1541,14 @@ const reverse = (x) => {
   }
   return y < Math.pow(-2, 31) || Math.pow(2, 31) < y ? 0 : y;
 };
+
+//Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0). Find two lines, which, together with the x-axis forms a container, such that the container contains the most water.
+
+ let maxWater = 0, start = 0, end = height.length-1;
+    
+    while(start < end) {
+        const area = (end - start) * Math.min(height[start], height[end]);
+        maxWater = Math.max(maxWater, area);
+        height[start] < height[end] ? start++ : end--;
+    }
+    return maxWater;
